babc9e1a579ce295200d345fae41f031a3ad73a5
Ugnius Mikaliunas <ugnius@betamedia.lt>
2021-06-20 14:37:20 +0300

    new type of galery

--- script.js
+++ script.js
@@ -1,112 +1,9 @@
 const mainContainer = document.getElementById('mainContainer')
 
-let images = [
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Raimundas',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Monika',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Donatas',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Rūta',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Agnė',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Lukas',
-        continue: false,
-    },
-    {
-        images: 4,
-        currentFrame: 1,
-        name: 'Julija',
-        continue: false,
-    },
-    {
-        images: 3,
-        currentFrame: 1,
-        name: 'Eglė',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Paulius',
-        continue: false,
-    },
-    {
-        images: 7,
-        currentFrame: 1,
-        name: 'Justė',
-        continue: false,
-    },
-    {
-        images: 3,
-        currentFrame: 1,
-        name: 'Nepažystu',
-        continue: false,
-    },
-    {
-        images: 6,
-        currentFrame: 1,
-        name: 'Airida',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Ugnė',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Marius',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Paulina',
-        continue: false,
-    },
-    {
-        images: 5,
-        currentFrame: 1,
-        name: 'Filomena',
-        continue: false,
-    },
-    {
-        images: 3,
-        currentFrame: 1,
-        name: 'Žyginta',
-        continue: false,
-    }
-]
+let names = ['Raimundas', 'Monika', 'Donatas', ' Rūta', 'Agnė', 'Lukas', 'Justė', 'Airida', 'Ugnė', 'Marius', 'Paulina', 'Filomena', 'Angelina', 'Žyginta', 'Julija', 'Rolandas', 'Ugnius', 'Jurga', 'Eglė']
 
 
-images.forEach((object, index) => {
+names.forEach((name, index) => {
     let imageContainer = document.createElement('div')
     imageContainer.classList.add('imageContainer')
     imageContainer.classList.add('col-12')
@@ -115,44 +12,21 @@ images.forEach((object, index) => {
     imageContainer.classList.add('col-lg-3')
     imageContainer.classList.add('position-relative')
     imageContainer.innerHTML = `<div class="wrapper">
-        <img class="image" src="./images/${index}.jpg" id="${index}" alt="">
-        <div class="nameWrap"><div class="name">${object.name}</div></div>
+        <img class="image" src="./images/bg/${index}.jpg" id="${index}" alt="">
+        <img class="image-hover" src="./images/${index}.gif" id="${index}" alt="">
+        <div class="nameWrap"><div class="name">${name}</div></div>
         <div class="over"></div>
         </div>`
     mainContainer.appendChild(imageContainer)
-    imageContainer.addEventListener('mouseover', function (event) { displayImages(event, object, index) })
-    imageContainer.addEventListener('click', function (event) { displayImages(event, object, index) })
-    imageContainer.addEventListener('mouseout', function (event) { displayShadow(event, object, index) })
-
+    imageContainer.addEventListener('click', displayImages)
 })
 
 
-function displayImages(event, object, index) {
-    object.continue=true
-    event.path[1].children[0].src = `images/${index}_${object.currentFrame}.jpg`
-
-    setTimeout(() => {
-        if (object.currentFrame < object.images) {
-            object.currentFrame++
-            if (object.continue) {
-                object.continue = false
-                displayImages(event, object, index)
-            }
-        } else {
-            object.currentFrame = 1
-            if (object.continue) {
-                object.continue = false
-                displayImages(event, object, index)
-            }
-        }
-    }, 800);
-
+function displayImages(event) {
+    console.log(event);
+    event.target.classList.add('opacity')
 
 }
-function displayShadow(event, object, index) {
-    object.continue = false
-    object.currentFrame = 1
-    event.path[1].children[0].src = `images/${index}.jpg`
-}
+
 
 
